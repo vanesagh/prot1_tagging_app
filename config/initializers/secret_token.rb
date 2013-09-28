62fc73fa03b18872bb6660b4dd733223ea45f811
vanesagh <vanesag@chalmers.se>
2013-09-28 22:07:05 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prot1TaggingApp::Application.config.secret_key_base = '96564f239c1cf4e4e2fa1a971f60515bfb0a84a55898c52144bb6853e55cb590a33f407c57ffa6306bdabd74547ab743f6db23e8d65a6b81bfb2cb91f5e5b311'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prot1TaggingApp::Application.config.session_store :cookie_store, key: '_prot1_tagging_app_session'
